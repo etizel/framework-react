8a95abe747527a81d1e5241137fbb324eb468db5
etiiizel <franciscoetizel@gmail.com>
2023-06-21 15:19:54 -0300

    page.tsx - app

---
+++ src/app/hooks/images.tsx
@@ -0,0 +1,11 @@
+import RandomImage from '@/components/hooks/RandomImage';
+
+export default function PageIamge() {
+  return (
+    <div>
+      <RandomImage />
+      <RandomImage />
+      <RandomImage />
+    </div>
+  );
+}
