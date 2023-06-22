84c9cb4738fc494df6d41e57b0cfabd415b00e9d
etiiizel <franciscoetizel@gmail.com>
2023-06-21 21:41:32 -0300

    hooks-imagens

--- src/app/hooks/page.tsx
+++ src/app/hooks/page.tsx
@@ -2,7 +2,11 @@ import RandomImage from './RandomImage';
 
 export default function PageImage() {
   return (
-    <div>
+    <div
+      className={`
+    flex justify-center items-center h-screen gap-5
+    `}
+    >
       <RandomImage />
       <RandomImage />
       <RandomImage />
