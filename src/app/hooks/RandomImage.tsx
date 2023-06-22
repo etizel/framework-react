9d8a4fde9d9c2405fe02ab8bb0038fa189b5cdca
etiiizel <franciscoetizel@gmail.com>
2023-06-21 21:31:59 -0300

    next.config.js

---
+++ src/app/hooks/RandomImage.tsx
@@ -0,0 +1,3 @@
+export default function RandomImage() {
+  return <div>RandomImage</div>;
+}
---
+++ src/app/hooks/page.tsx
@@ -0,0 +1,11 @@
+import RandomImage from './RandomImage';
+
+export default function PageImage() {
+  return (
+    <div>
+      <RandomImage />
+      <RandomImage />
+      <RandomImage />
+    </div>
+  );
+}
