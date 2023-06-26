24931197cc152c0c0d4827d2c68153488dcdf3d9
etiiizel <franciscoetizel@gmail.com>
2023-06-26 15:00:29 -0300

    import Image from next/image

--- src/app/hooks/RandomImage.tsx
+++ src/app/hooks/RandomImage.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 export default function RandomImage() {
   const url = 'https://source.unsplash.com/featured/300x300?';
   var pesquisa: string = '';
@@ -25,6 +26,7 @@ export default function RandomImage() {
 
   return (
     <div className="flex flex-col gap-3 border border-zinc-500 p-7 rounded-md">
+      <Image src={finalUrl()} height={300} width={300} alt="Imagem" />
       <div className="flex gap-5">
         {RandomBtn('abstract')}
         {RandomBtn('city')}
