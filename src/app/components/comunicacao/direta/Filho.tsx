ca2b7e4440c103718f3579f2d0058c6236808ede
etiiizel <franciscoetizel@gmail.com>
2023-07-09 13:12:41 -0300

    comunicacao entre componentes

--- src/app/components/comunicacao/direta/Filho.tsx
+++ src/app/components/comunicacao/direta/Filho.tsx
@@ -1,3 +1,10 @@
 export default function Filho() {
-  return <div></div>;
+  return (
+    <div
+      className={`
+    flex justify-center items-center
+    bg-blue-500 rounded-md p-5
+  `}
+    ></div>
+  );
 }
--- src/app/comunicacao/page.tsx
+++ src/app/comunicacao/page.tsx
@@ -3,7 +3,7 @@ import Avo from '../components/comunicacao/direta/Avo';
 export default function PageStright() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1>Comunicação Direta</h1>
+      <h1 className="text-5xl mb-10">Comunicação Direta</h1>
       <Avo />
     </div>
   );
