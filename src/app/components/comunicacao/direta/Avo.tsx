0a976f0184a0d0cd652c8c872bacf56390d3fadc
etiiizel <franciscoetizel@gmail.com>
2023-07-09 13:17:26 -0300

    comunicacao entre componentes

--- src/app/components/comunicacao/direta/Avo.tsx
+++ src/app/components/comunicacao/direta/Avo.tsx
@@ -2,7 +2,12 @@ import Pai from './Pai';
 
 export default function Avo() {
   return (
-    <div className="flex gap-5">
+    <div
+      className={`
+    flex gap-5 p-5
+    bg-purple-500 border border-white rounded-md
+    `}
+    >
       <Pai />
       <Pai />
       <Pai />
