faa25cef026e628fc251d48df497bd779e9e602c
etiiizel <franciscoetizel@gmail.com>
2023-07-10 13:24:08 -0300

    passando dados de forma direta

--- src/app/comunicacao/page.tsx
+++ src/app/comunicacao/page.tsx
@@ -2,9 +2,10 @@ import Avo from '../components/comunicacao/direta/Avo';
 
 export default function PageStright() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center gap-5 h-screen">
       <h1 className="text-5xl mb-10">Comunicação Direta</h1>
       <Avo nome="Pierre" sobrenome="Alexander" />
+      <Avo nome="Scobar" sobrenome="Landrino" />
     </div>
   );
 }
